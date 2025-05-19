a9f84408d46855af514987467790d5fd1a12b5d2
GabboClown <russogab269@gmail.com>
2025-05-19 11:22:27 +0200

    Fix

--- insertnew.php
+++ insertnew.php
@@ -14,7 +14,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Gestione Cartelle Cliniche | Inserisci nuovo <?php echo htmlspecialchars($_GET["admin"]) == "true" ? "admin" : "cittadino" ?></title>
+  <title>Gestione Cartelle Cliniche | Inserisci nuovo <?php echo htmlspecialchars($_GET["admin"]) == "true" ? "admin" : "paziente" ?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
--- showdata.php
+++ showdata.php
@@ -22,7 +22,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Gestione Cartelle Cliniche | Database <?php echo $isAdmin ? "Admin" : "Cittadini";?></title>
+  <title>Gestione Cartelle Cliniche | Database <?php echo $isAdmin ? "Admin" : "Pazienti";?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
